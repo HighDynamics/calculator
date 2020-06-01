6545651934620d7e175cabf340a7400500a4ccb5
Daniel <highdynamics@gmail.com>
2020-06-01 10:46:25 -0500

    add number buttons

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import ReactDOM from 'react'
 import './App.css';
 
-function App() {
+function Output(){
+  return(
+    <>
+    </>
+  );
+}
+
+function UtilityButtons(){
+
+}
+
+function Operators(){
+
+}
+function NumberList(props){
+  const numbers = props.numbers;
+  const buttonNumbers = numbers.map((number) =>
+    <button key={number.toString()}>{number}</button>
+  );
+  return(
+    <div>{buttonNumbers}</div>
+  )
+}
+function Numbers(){
+  const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+  return(
+    <>
+      <NumberList numbers={numbers}/>
+    </>
+  );
+}
+
+function App(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Numbers />
+    </>
   );
 }
 
