110fa08102548afb98e311bd1fa4c7f9e7db452a
Daniel <highdynamics@gmail.com>
2020-06-01 21:43:41 -0500

    mid experiment with passing props

--- src/App.js
+++ src/App.js
@@ -2,9 +2,10 @@ import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react'
 import './App.css';
 
-function Output(){
+function Output(props){
   return(
     <>
+      {input}
     </>
   );
 }
@@ -23,7 +24,7 @@ function UtilityButtons(props){
   );
 }
 //All Clear, negative/positive, percentage
-function Utilities(){
+function Utilities(props){
   const utilities = ['AC', '+/-', '%']
   return(
     <UtilityButtons utilities={utilities} />
@@ -44,8 +45,8 @@ function OperatorList(props){
   );
 }
 //operators on a calculator: + - * %
-function Operators(){
-  const operators = ['+', '-', 'x', '%']
+function Operators(props){
+  const operators = ['+', '-', 'x', '%', '=']
   return(
     <>
       <OperatorList operators={operators} />
@@ -68,21 +69,23 @@ function NumberList(props){
   )
 }
 //numbers on a calculator
-function Numbers(){
+function Numbers(props){
   const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
   return(
     <>
-      <NumberList numbers={numbers}/>
+      <NumberList numbers={numbers} />
     </>
   );
 }
-
+//MOST RECENT ----- ATTEMPTING TO PASS PROPS FOR DISPLAY
 function App(){
+  const [input, setInput] = useState('')
   return (
     <>
-      <Numbers />
-      <Operators />
-      <Utilities />
+      <Output input={input}/>
+      <Numbers setInput={setInput}/>
+      <Operators setInput={setInput}/>
+      <Utilities setInput={setInput}/>
     </>
   );
 }
