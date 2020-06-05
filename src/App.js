b9cdcf8fa5b4f2d5d620d465e1a9d119ab650378
Daniel <highdynamics@gmail.com>
2020-06-05 12:29:43 -0500

    add functionality to some operators and utitlies

--- src/App.js
+++ src/App.js
@@ -2,8 +2,15 @@ import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react'
 import './App.css';
 
+function OutputHistory(props){
+  return(
+    <div>{sessionStorage.expression}</div>
+  );
+}
+
 function Output(props){
   const input = props.input;
+//parseFloat makes initial 0 disappear! Hooray
   return(
     <>
       {parseFloat(input, 10)}
@@ -11,10 +18,33 @@ function Output(props){
   );
 }
 //get utilities from props and render
-function UtilityButtons(props){
+function UtilityList(props){
   const utilities = props.utilities;
+  const input = props.input;
+  const setInput = props.setInput;
+  function clickUtility(utility){
+    switch(utility){
+      case 'AC':
+        sessionStorage.expression = '';
+        setInput('0');
+        break;
+      case 'C':
+        setInput('0');
+        break;
+      case '+/-':
+        if(input.charAt(0) !== '-'){
+          setInput('-' + input);
+        }else{
+          setInput(input.substring(1));
+        };
+        break;
+    }
+  }
   const utilityButtons = utilities.map((utility) =>
-  <button key={utility} id={utility}>
+  <button
+    key={utility}
+    id={utility}
+    onClick={() => clickUtility(utility)}>
     {utility}
   </button>
   );
@@ -24,21 +54,53 @@ function UtilityButtons(props){
     </>
   );
 }
-//All Clear, negative/positive, percentage
+//All clear, clear, negative/positive, percentage
 function Utilities(props){
-  const utilities = ['AC', '+/-', '%']
+  const utilities = ['AC', 'C', '+/-', '%']
+  const input = props.input;
+  const setInput = props.setInput;
   return(
-    <UtilityButtons utilities={utilities} />
+    <UtilityList
+      utilities={utilities}
+      input={input}
+      setInput={setInput}
+    />
   );
 }
 //get operators from props and render
 function OperatorList(props){
   const operators = props.operators;
+  const input = props.input;
+  const setInput = props.setInput;
+//Saves input, concatenates to previous input(if any); reset state
+  function saveExpression(operator){
+    switch(operator){
+      case '+':
+        sessionStorage.expression = sessionStorage.expression + input + ' + ';
+        setInput('0');
+        break;
+      case '-':
+        sessionStorage.expression = sessionStorage.expression + input + ' - ';
+        setInput('0');
+        break;
+      case 'x':
+        sessionStorage.expression = sessionStorage.expression + input + ' * ';
+        setInput('0');
+        break;
+      case String.fromCharCode('0x00F7'):
+        sessionStorage.expression = sessionStorage.expression + input + ' ' + String.fromCharCode('0x00F7') + ' ';
+        setInput('0');
+        break;
+    }
+  }
   const operatorButtons = operators.map((operator) =>
-    <button key={operator} id={operator}>
-      {operator}
+    <button
+      key={operator}
+      id={operator}
+      onClick={() => saveExpression(operator)}>
+        {operator}
     </button>
-  )
+  );
   return(
     <>
       {operatorButtons}
@@ -47,15 +109,21 @@ function OperatorList(props){
 }
 //operators on a calculator: + - * %
 function Operators(props){
-  const operators = ['+', '-', 'x', '%', '=']
+  const input = props.input;
+  const setInput = props.setInput;
+  const operators = ['+', '-', 'x', String.fromCharCode('0x00F7'), '=']
   return(
     <>
-      <OperatorList operators={operators} />
+      <OperatorList
+        operators={operators}
+        input={input}
+        setInput={setInput}
+      />
     </>
   );
 }
 
-//get numbers from props and render
+//Get numbers from props and render. Add functionality
 function NumberList(props){
   const numbers = props.numbers;
   const input = props.input;
@@ -81,19 +149,24 @@ function Numbers(props){
   const setInput = props.setInput;
   return(
     <>
-      <NumberList numbers={numbers} setInput={setInput} input={input}/>
+      <NumberList
+        numbers={numbers}
+        setInput={setInput}
+        input={input}
+      />
     </>
   );
 }
-//MOST RECENT ----- ATTEMPTING TO PASS PROPS FOR DISPLAY
+
 function App(){
   const [input, setInput] = useState('0')
   return (
     <>
+      <OutputHistory />
       <Output input={input}/>
       <Numbers setInput={setInput} input={input}/>
-      <Operators setInput={setInput}/>
-      <Utilities setInput={setInput}/>
+      <Operators setInput={setInput} input={input}/>
+      <Utilities setInput={setInput} input={input}/>
     </>
   );
 }
