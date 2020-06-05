7231f7744659e2628b9257dc445be55470e99b75
Daniel <highdynamics@gmail.com>
2020-06-05 17:36:18 -0500

    Updates

---
+++ precache-manifest.210342e8229dde33e6deb885ea6ab38d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ceccfe0d1b39554a2d717e1d18f1f26",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "968a9901efc8d840f1f3",
+    "url": "/calculator/static/css/main.9c7b0f32.chunk.css"
+  },
+  {
+    "revision": "aebe79fc21e6621c75c3",
+    "url": "/calculator/static/js/2.5b45ed2b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/calculator/static/js/2.5b45ed2b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "968a9901efc8d840f1f3",
+    "url": "/calculator/static/js/main.0579846a.chunk.js"
+  },
+  {
+    "revision": "53122c127a7fe401835e",
+    "url": "/calculator/static/js/runtime-main.6de0c573.js"
+  }
+]);
